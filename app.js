b8035d1b1e2d7fb8db7d97120f93b466ef10e7b0
Camillemtd <camille_mtd@hotmail.com>
2023-09-14 16:58:40 -0400

    fiste commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const morgan = require('morgan')
+const favicon = require('serve-favicon')
+const sequelize = require('./src/db/sequelize')
+const bodyParser = require('body-parser')
+
+
+const app = express()
+const port = 3000
+
+app
+	.use(favicon(__dirname + '/favicon.ico'))
+	.use(morgan('dev'))
+	.use(bodyParser.json())
+
+sequelize.initDb
+
+//const findAllPokemons = require('./src/routes/FindAllPokemons')
+//findAllPokemons(app)
+//en dessous syntaxe simplifiée 
+require('./src/routes/FindAllPokemons')(app)
+require('./src/routes/FindPokemonByPk')(app)
+require('./src/routes/CreatePokemon')(app)
+require('./src/routes/UpdatePokemon')(app)
+require('./src/routes/DeletePokemon')(app)
+
+
+app.listen(port, () => console.log(`Notre application Node est démarré sur : http://localhost:${port}`))
---
+++ src/models/pokemons.js
@@ -0,0 +1,42 @@
+module.exports = (sequelize, DataTypes) => {
+	return sequelize.define('Pokemon', {
+	  id: {
+		type: DataTypes.INTEGER,
+		primaryKey: true,
+		autoIncrement: true
+	  },
+	  name: {
+		type: DataTypes.STRING,
+		allowNull: false
+	  },
+	  hp: {
+		type: DataTypes.INTEGER,
+		allowNull: false
+	  },
+	  cp: {
+		type: DataTypes.INTEGER,
+		allowNull: false
+	  },
+	  picture: {
+		type: DataTypes.STRING,
+		allowNull: false
+	  },
+	  types: {
+		type: DataTypes.STRING,
+		allowNull: false,
+		// Getter : Base de données => API Rest
+		get() {
+			return this.getDataValue('types').split(',')
+		},
+		// Setter : API Rest => Base de données
+		set(types) {
+			this.setDataValue('types', types.join())
+		}
+	  }
+	}, {
+	  timestamps: true,
+	  createdAt: 'created',
+	  updatedAt: false
+	})
+  }
+
