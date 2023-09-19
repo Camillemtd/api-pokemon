bdf4124aa6a053278c3282a0badf4b8982c10831
Camillemtd <camille_mtd@hotmail.com>
2023-09-19 15:03:09 -0400

    add database connection

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -1,39 +1,55 @@
-const { Sequelize, DataTypes } = require('sequelize')
-const PokemonModel = require('../models/pokemons')
-const pokemons = require('./mock-pokemon')
-const UserModel = require('../models/user')
+const { Sequelize, DataTypes } = require("sequelize");
+const PokemonModel = require("../models/pokemons");
+const pokemons = require("./mock-pokemon");
+const UserModel = require("../models/user");
 
-  
-const sequelize = new Sequelize('pokedex', 'root', '', {
-  host: 'localhost',
-  dialect: 'mariadb',
-  dialectOptions: {
-    timezone: 'Etc/GMT-2',
-  },
-  logging: false
-})
-  
-const Pokemon = PokemonModel(sequelize, DataTypes)
-const User = UserModel(sequelize, DataTypes)
-  
+let sequelize;
+if (process.env.NODE_ENV === production) {
+  const sequelize = new Sequelize(
+    "tvfos9ld3ymssbga",
+    "ettnbf40nlg16ot1",
+    "wxxgha7v6a32z5ac",
+    {
+      host: "un0jueuv2mam78uv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+      dialect: "mariadb",
+      dialectOptions: {
+        timezone: "Etc/GMT-2",
+      },
+      logging: true,
+    }
+  );
+} else {
+  sequelize = new Sequelize("pokedex", "root", "", {
+    host: "localhost",
+    dialect: "mariadb",
+    dialectOptions: {
+      timezone: "Etc/GMT-2",
+    },
+    logging: true,
+  });
+}
+
+const Pokemon = PokemonModel(sequelize, DataTypes);
+const User = UserModel(sequelize, DataTypes);
+
 const initDb = () => {
-  return sequelize.sync({force: true}).then(_ => {
-    pokemons.map(pokemon => {
+  return sequelize.sync().then((_) => {
+    pokemons.map((pokemon) => {
       Pokemon.create({
         name: pokemon.name,
         hp: pokemon.hp,
         cp: pokemon.cp,
         picture: pokemon.picture,
-        types: pokemon.types
-      }).then(pokemon => console.log(pokemon.toJSON()))
-    })
-    
-   
+        types: pokemon.types,
+      }).then((pokemon) => console.log(pokemon.toJSON()));
+    });
 
-    console.log('La base de donnée a bien été initialisée !')
-  })
-}
-  
-module.exports = { 
-  initDb, Pokemon, User
-}
+    console.log("La base de donnée a bien été initialisée !");
+  });
+};
+
+module.exports = {
+  initDb,
+  Pokemon,
+  User,
+};
